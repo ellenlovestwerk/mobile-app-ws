98910c7dbb54ae0f84f4f6b80a7ce87deeefe9ed
ellenlovestwerk <ellenlovestwerk@gmail.com>
2021-08-04 16:26:25 -0700

    UserDto

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -3,6 +3,7 @@ package com.appsdeveloperblog.app.ws.ui.controller;
 import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
 import com.appsdeveloperblog.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,8 +17,11 @@ public class UserController {
     public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
         UserRest returnValue = new UserRest();
         UserDto userDto = new UserDto();
+        BeanUtils.copyProperties(userDetails, userDto);
 
-        return null;
+        UserDto createdUser = userService.createUser(userDto);
+        BeanUtils.copyProperties(createdUser, returnValue);
+        return returnValue;
     }
     @PutMapping
     public String updateUser() {
