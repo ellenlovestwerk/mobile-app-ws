e73a8ef2229254b5e55bae1ae244365c5212c725
ellenlovestwerk <ellenlovestwerk@gmail.com>
2021-08-04 16:58:37 -0700

    UserServiceImpl

--- src/main/java/com/appsdeveloperblog/app/ws/io/entity/UserEntity.java
+++ src/main/java/com/appsdeveloperblog/app/ws/io/entity/UserEntity.java
@@ -1,17 +1,30 @@
-package com.appsdeveloperblog.app.ws.shared.dto;
+package com.appsdeveloperblog.app.ws.io.entity;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import java.io.Serializable;
 
-public class UserDto implements Serializable {
-
+@Entity(name="users")
+public class UserEntity implements Serializable {
+    private static final long serialVersionID = 1L;
+    @Id
+    @GeneratedValue
     private long id;
+
+    @Column(nullable=false)
     private String userId;
-    private String firsName;
+    @Column(nullable=false, length =50)
+    private String firstName;
+    @Column(nullable=false, length =50)
     private String lastName;
+    @Column(nullable=false, length =120)
     private String email;
-    private String password;
+    @Column(nullable=false)
     private String encryptedPassword;
     private String emailVerificationToken;
+    @Column(nullable=false)
     private Boolean emailVerificationStatus = false;
 
     public long getId() {
@@ -30,12 +43,12 @@ public class UserDto implements Serializable {
         this.userId = userId;
     }
 
-    public String getFirsName() {
-        return firsName;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setFirsName(String firsName) {
-        this.firsName = firsName;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public String getLastName() {
@@ -52,14 +65,6 @@ public class UserDto implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getEncryptedPassword() {
--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -1,14 +1,19 @@
 package com.appsdeveloperblog.app.ws.ui.controller;
 
+import com.appsdeveloperblog.app.ws.service.UserService;
 import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
 import com.appsdeveloperblog.app.ws.ui.model.request.UserDetailsRequestModel;
 import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users") //http://localhost:8080/users
 public class UserController {
+
+    @Autowired
+    UserService userService;
     @GetMapping
     public String getUser() {
         return "get User";
