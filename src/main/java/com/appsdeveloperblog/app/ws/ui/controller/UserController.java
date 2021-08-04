1bf0754b9f32cea1811b294ceed94b3cc4a9cce4
ellenlovestwerk <ellenlovestwerk@gmail.com>
2021-08-04 15:32:16 -0700

    first commit

---
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.appsdeveloperblog.app.ws.ui.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("users") //http://localhost:8080/users
+public class UserController {
+    @GetMapping
+    public String getUser() {
+        return "get User";
+    }
+    @PostMapping
+    public String createUser() {
+        return "create User";
+    }
+    @PutMapping
+    public String updateUser() {
+        return "update User";
+    }
+    @DeleteMapping
+    public String deleteUser() {
+        return "delete User";
+    }
+}
